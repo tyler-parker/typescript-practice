84945ac8010d41c6fab15f585048e6d6566f0c57
Tyler Parker <tparker42d2@gmail.com>
2021-12-21 11:54:34 -0700

    covered interfaces, assertions, functions and classes.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,66 @@
+//Basic types
 var id = 5;
-console.log(id);
+var company = 'Tyler Corp';
+var isPublished = true;
+var x = 'Hello';
+var age;
+var arrayOfNums = [1, 2, 3, 4, 5];
+var varietyOfTypes = [1, false, 'true'];
+// Tuple example
+var person = [1, 'Tyler', false];
+// Tuple Array
+var employee;
+employee = [
+    [1, 'Tyler'],
+    [2, 'Mia'],
+    [3, 'Louie']
+];
+// Union
+var pid;
+// Enum
+var numberedEnum;
+(function (numberedEnum) {
+    numberedEnum[numberedEnum["Up"] = 1] = "Up";
+    numberedEnum[numberedEnum["Down"] = 2] = "Down";
+    numberedEnum[numberedEnum["Left"] = 3] = "Left";
+    numberedEnum[numberedEnum["Right"] = 4] = "Right";
+})(numberedEnum || (numberedEnum = {}));
+var stringEnum;
+(function (stringEnum) {
+    stringEnum["Up"] = "Up";
+    stringEnum["Down"] = "Down";
+    stringEnum["Left"] = "Left";
+    stringEnum["Right"] = "Right";
+})(stringEnum || (stringEnum = {}));
+var user = {
+    id: 1,
+    name: 'John'
+};
+// Type Assertion
+var cid = 1;
+var customerIdAsAssertion = cid; // this looks better
+var customerIdTagAssertion = cid; // same thing, but doesn't look as great
+// Functions
+function addNumb(x, y) {
+    return x + y;
+}
+var user2 = {
+    id: 1,
+    name: 'John'
+};
+var add = function (x, y) { return x + y; };
+var sub = function (x, y) { return x - y; };
+var Person = /** @class */ (function () {
+    function Person(id, name) {
+        console.log(123);
+        this.id = id;
+        this.name = name;
+    }
+    Person.prototype.register = function () {
+        return "".concat(this.name, " is now registered");
+    };
+    return Person;
+}());
+var tyler = new Person(1, 'Tyler Parker');
+console.log(tyler.register());
+console.log(tyler);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,113 @@
+//Basic types
 let id: number = 5
+let company: string = 'Tyler Corp'
+let isPublished: boolean = true
+let x: any = 'Hello'
+let age: number
 
-console.log(id);
+let arrayOfNums: number[] = [1,2,3,4,5]
+let varietyOfTypes: any[] = [1,false,'true']
+
+// Tuple example
+let person: [number, string, boolean] = [1, 'Tyler', false]
+// Tuple Array
+let employee: [number, string][]
+
+employee = [
+    [1, 'Tyler'],
+    [2, 'Mia'],
+    [3, 'Louie']
+]
+
+// Union
+let pid: string | number
+
+// Enum
+enum numberedEnum {
+    Up = 1, // from here, the enum IDs will increment from 1
+    Down,
+    Left,
+    Right
+}
+
+enum stringEnum {
+    Up = 'Up', // from here, the enum IDs will increment from 1
+    Down = 'Down',
+    Left = 'Left',
+    Right = 'Right'
+}
+
+// Objects
+type User = {
+    id: number,
+    name: string
+}
+
+const user: User = {
+    id: 1, 
+    name: 'John'
+}
+
+// Type Assertion
+let cid: any = 1
+let customerIdAsAssertion = cid as number // this looks better
+let customerIdTagAssertion = <number>cid // same thing, but doesn't look as great
+
+// Functions
+function addNumb(x: number, y: number): number {
+    return x + y
+}
+
+// Interface
+interface UserInterface {
+    id: number
+    name: string
+    age?: number
+}
+
+const user2: UserInterface = {
+    id: 1, 
+    name: 'John'
+}
+
+interface MathFunc {
+    (x: number, y: number): number
+}
+
+const add: MathFunc = (x: number, y: number): number => x + y
+const sub: MathFunc = (x: number, y: number): number => x - y
+
+// Classes
+
+// when declaring properties,
+// private limits its access to only its residing class
+// protected limits access to its residing class or any other extended class
+// nothing is an implicit 'public' which allows access from anywhere
+
+interface PersonInterface {
+    id: number
+    name: string
+    register(): string
+}
+
+class Person implements PersonInterface {
+    id: number
+    name: string
+
+
+    constructor(id: number, name: string){
+        console.log(123);
+        this.id = id
+        this.name = name
+    }
+
+    register(){
+        return `${this.name} is now registered`
+    }
+}
+
+const tyler = new Person(1, 'Tyler Parker')
+
+console.log(tyler.register());
+
+console.log(tyler);
